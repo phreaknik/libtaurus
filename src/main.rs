77144f202fc84dbf62224b3700de7664799b122d
John Boyd <john@coldnoise.net>
2025-03-29 17:00:00 +0000

    Subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use clap::{arg, command, ArgMatches};
+use clap::{arg, command, ArgMatches, Command};
+use cordelia_p2p::peer_db::PeerDB;
 use etcetera::{base_strategy::choose_native_strategy, BaseStrategy};
 use std::path::PathBuf;
 use tokio;
-use tracing::{info, span, Level};
+use tracing::info;
 use tracing_subscriber::EnvFilter;
 
 /// Application configuration details
@@ -11,11 +12,19 @@ struct Config<'a> {
     p2p_cfg: cordelia_p2p::Config<'a>,
 }
 
+/// Main cordelia CLI application
 #[tokio::main]
 async fn main() {
     // Parse CLI arguments
-    let args = parse_args();
+    match parse_cli_args().subcommand() {
+        Some(("run", sub_args)) => run_cmd(sub_args).await,
+        Some(("list-peers", sub_args)) => list_peers_cmd(sub_args),
+        _ => unreachable!("Exausted list of subcommands and subcommand_requred prevents 'None'"),
+    }
+}
 
+/// Command to start node and connect to the network
+async fn run_cmd(args: &ArgMatches) {
     // Set up a subscriber to capture logs
     setup_logger(&args);
 
@@ -27,12 +36,40 @@ async fn main() {
     cordelia_p2p::run(&cfg.p2p_cfg).await.unwrap();
 }
 
+/// Command to list peers
+fn list_peers_cmd(args: &ArgMatches) {
+    // Set up a subscriber to capture logs
+    setup_logger(&args);
+
+    // Open the peer database
+    let peer_dir = parse_data_dir(args).join("p2p/peer_db/");
+    let db = PeerDB::open_read_only(peer_dir).expect("failed to open peer database");
+
+    // Print the peers
+    db.print_peers(args.get_one("max").map(|x| *x));
+}
+
 /// Parse CLI args
-fn parse_args() -> ArgMatches {
+fn parse_cli_args() -> ArgMatches {
     command!() // initialize CLI with details from cargo.toml
-        .arg(arg!(-v --verbosity ... "Increase verbosity level").required(false))
-        .arg(arg!(-d --data_dir <PATH> "Specify data directory").required(false))
-        .arg(arg!(--static_peer <MULTIADDR> "Specify static peer to connect to").required(false))
+        .subcommand(
+            Command::new("run")
+                .about("Connect to the p2p network and join consensus")
+                .arg(arg!(-v --verbosity ... "Increase verbosity level").required(false))
+                .arg(arg!(-d --data_dir <PATH> "Specify data directory").required(false))
+                .arg(
+                    arg!(--static_peer <MULTIADDR> "Specify static peer to connect to")
+                        .required(false),
+                ),
+        )
+        .subcommand(
+            Command::new("list-peers")
+                .about("Lists all peers saved in the peer database")
+                .arg(arg!(-v --verbosity ... "Increase verbosity level").required(false))
+                .arg(arg!(-d --data_dir <PATH> "Specify data directory").required(false))
+                .arg(arg!(-n --max <COUNT> "Max number of peers to list").required(false)),
+        )
+        .subcommand_required(true)
         .get_matches()
 }
 
@@ -46,13 +83,17 @@ fn setup_logger<'a>(args: &'a ArgMatches) {
     tracing_subscriber::fmt().with_env_filter(log_filter).init();
 }
 
+/// Determine system directories for the application to use
+fn parse_data_dir(args: &ArgMatches) -> PathBuf {
+    let app_dirs = choose_native_strategy().expect("failed to build application directories");
+    args.get_one::<String>("data_dir")
+        .map(|s| PathBuf::from(s))
+        .unwrap_or(app_dirs.data_dir().join("cordelia/"))
+}
+
 /// Build application config from parsed CLI args
 fn build_cfg<'a>(args: &'a ArgMatches) -> Config<'a> {
-    let app_dirs = choose_native_strategy().expect("failed to build application directories");
-    let data_dir = args
-        .get_one::<String>("data_dir")
-        .map(|s| PathBuf::from(s))
-        .unwrap_or(app_dirs.data_dir().join("cordelia/"));
+    let data_dir = parse_data_dir(args);
     Config {
         p2p_cfg: build_p2p_cfg(data_dir.join("p2p/"), args),
     }
