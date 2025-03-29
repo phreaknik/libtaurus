c3c7ed49878a2df5b5ee1e7e544664e1d8862f5b
John Boyd <john@coldnoise.net>
2025-03-29 03:00:00 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,47 @@
-use cordelia_p2p::{Behaviour, Config};
+use cordelia_p2p::Config;
 use futures::prelude::*;
 use libp2p::multiaddr::Protocol;
 use libp2p::swarm::{SwarmBuilder, SwarmEvent};
 use libp2p::{identity, Multiaddr, PeerId};
-use std::error::Error;
 use std::net::Ipv4Addr;
+use tokio;
 
-#[async_std::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() {
     let local_key = identity::Keypair::generate_ed25519();
     let local_peer_id = PeerId::from(local_key.public());
     println!("Local peer id: {local_peer_id:?}");
 
-    // Configure the cordelia network behaviour
-    let behaviour = Behaviour::new(Config::new(local_key.public()));
-
+    // Build the swarm
     // TODO: pick a different transport. development_transport() has features we likely don't want,
     // e.g. noise encryption
-    let transport = libp2p::development_transport(local_key).await?;
-
-    let mut swarm =
-        SwarmBuilder::with_async_std_executor(transport, behaviour, local_peer_id).build();
+    let mut swarm = SwarmBuilder::with_tokio_executor(
+        libp2p::development_transport(local_key.clone())
+            .await
+            .expect("failed to construct transport"),
+        cordelia_p2p::Behaviour::new(Config::new(local_key.public())),
+        local_peer_id,
+    )
+    .build();
 
     // Listen for inbound connections
-    swarm.listen_on(
-        Multiaddr::empty()
-            .with(Protocol::Ip4(Ipv4Addr::UNSPECIFIED))
-            .with(Protocol::Tcp(0)),
-    )?;
+    swarm
+        .listen_on(
+            Multiaddr::empty()
+                .with(Protocol::Ip4(Ipv4Addr::UNSPECIFIED))
+                .with(Protocol::Tcp(0)),
+        )
+        .expect("failed to build swarm");
 
     // Dial the peer identified by the multi-address given as the second
     // command-line argument, if any.
     if let Some(addr) = std::env::args().nth(1) {
-        let remote: Multiaddr = addr.parse()?;
-        swarm.dial(remote)?;
-        println!("Dialed {addr}")
+        let remote: Multiaddr = addr.parse().expect("bad address");
+        if swarm.dial(remote).is_err() {
+            println!("error: failed to dial peer");
+        } else {
+            println!("Dialed {addr}")
+        }
     }
 
     loop {
