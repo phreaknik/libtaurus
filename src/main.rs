aaf841d2d12137562f4d80019821a729c57144fb
John Boyd <john@coldnoise.net>
2025-03-29 11:00:00 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::path::PathBuf;
 
 use clap::{arg, command, ArgMatches};
-use etcetera::{base_strategy::choose_native_strategy, base_strategy::Xdg, BaseStrategy};
+use etcetera::{base_strategy::choose_native_strategy, BaseStrategy};
 use tokio;
 
 /// Application configuration details
 struct Config<'a> {
-    p2p_data_dir: PathBuf,
+    /// P2P client configuration
     p2p_cfg: cordelia_p2p::Config<'a>,
 }
 
@@ -25,7 +25,7 @@ async fn main() {
 /// Parse CLI args
 fn parse_args() -> ArgMatches {
     command!() // initialize CLI with details from cargo.toml
-        .arg(arg!(--peer_db_path <PATH>).required(false))
+        .arg(arg!(--data_dir <PATH>).required(false))
         .arg(arg!(--static_peer <MULTIADDR>).required(false))
         .get_matches()
 }
@@ -33,20 +33,19 @@ fn parse_args() -> ArgMatches {
 /// Build application config from parsed CLI args
 fn build_cfg<'a>(args: &'a ArgMatches) -> Config<'a> {
     let app_dirs = choose_native_strategy().expect("failed to build application directories");
-    let data_dir = app_dirs.data_dir().join("cordelia/");
+    let data_dir = args
+        .get_one::<String>("data_dir")
+        .map(|s| PathBuf::from(s))
+        .unwrap_or(app_dirs.data_dir().join("cordelia/"));
     Config {
-        p2p_data_dir: data_dir.join("p2p/"),
-        p2p_cfg: build_p2p_cfg(&data_dir, args),
+        p2p_cfg: build_p2p_cfg(data_dir.join("p2p/"), args),
     }
 }
 
 /// Build ['cordelia-p2p'] config from parsed CLI args
-fn build_p2p_cfg<'a>(p2p_data_dir: &PathBuf, args: &'a ArgMatches) -> cordelia_p2p::Config<'a> {
+fn build_p2p_cfg<'a>(p2p_data_dir: PathBuf, args: &'a ArgMatches) -> cordelia_p2p::Config<'a> {
     cordelia_p2p::Config {
         static_peer: args.get_one::<String>("static_peer").map(move |s| &s[..]),
-        peer_db_path: args
-            .get_one::<String>("peer_db_path")
-            .map(|s| PathBuf::from(s))
-            .unwrap_or(p2p_data_dir.join("peer_db/")),
+        data_dir: p2p_data_dir,
     }
 }
