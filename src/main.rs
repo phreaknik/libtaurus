dad3a4d6df1a5d5c4dc767a55bfa58893ff6893e
John Boyd <john@coldnoise.net>
2025-04-10 08:00:00 +0000

    Add library docs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 #![feature(iterator_try_collect)]
 
+//! An implementation of the Cordelia distributed ledger protocol. Cordelia is a combination of
+//! proven technologies, which when combined facilitate a scalable, fungible, and programmable
+//! payment system.
+//!
+//! Protocol overview:
+//!   * Proof-of-work provides objective sybil resistance for strong network security.
+//!   * Avalanche Consensus provides quick consensus with optimal finalization times.
+//!   * zkRollups to scale transaction throughput and provide fungibility.
+//!
+//! Essentially, the protocol behaves as follows:
+//! * miners build blocks with proof-of-work. Unlike other networks, a block does not commit to its
+//!   parent(s). That job belongs to the vertex.
+//! * miners do not get rewarded directly. Reward requires Proof-of-work AND participation in
+//!   Avalanche consensus. Thus, miners also run a node to participate in consensus.
+//! * nodes select blocks for inclusion in the dag. They build a vertex which links a block to some
+//!   parents in the DAG, and share that vertex with peers.
+//! * nodes are selected randomly to participate in avalanche queries. Only nodes who recently mined
+//!   a block with sufficient proof-of-work are selected.
+//! * once a node has served avalanche requests for many peers, it may submit a payout transaction
+//!   for every block its mined.
+//! * nodes keep a record of peers who are participating in Avalanche consensus. Nodes will only
+//!   vote favorably for a payout transaction, if they deem the payee to have been an active
+//!   avalanche participant AND have submitted appropriate proofs-of-work.
+
 mod consensus;
 mod http;
 mod miner;
